1b21fdad54501be44a04325d2c1d3e49bea47c46
JorgeEscalonas <escalonajorge73@gmail.com>
2025-11-28 19:57:40 -0400

    Creación y configuración del proyecto

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="p-8">
+    <h1 class="text-3xl font-bold underline text-green-600">
+      Dashboard
+    </h1>
+  </div>
+</template>
---
+++ src/views/login/LoginView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex min-h-screen items-center justify-center bg-gray-100">
+    <div class="p-8 bg-white shadow-md rounded-lg">
+      <h1 class="text-2xl font-bold text-blue-600 mb-4">Login</h1>
+      <p class="text-gray-700">Tailwind CSS is working!</p>
+    </div>
+  </div>
+</template>
